9e668582e11cc99929a4fed25c58661dbac41167
sdk-generator <smartx@info.com>
2023-02-17 07:19:03 +0000

    generate schema from new schema

--- cloudtower/__init__.py
+++ cloudtower/__init__.py
@@ -3,7 +3,7 @@
 # flake8: noqa
 from __future__ import absolute_import
 
-__version__ = "2.5.0"
+__version__ = "2.6.0"
 
 # import apis into sdk package
 from cloudtower.api.alert_api import AlertApi
